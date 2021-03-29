f73c631c6330f1af52da64b29bd43972f5faed89
Jérome Perrin <perrinjerome@gmail.com>
2021-03-29 13:48:49 +0000

    cli: support multiple files

--- cli/theia_open/cli.py
+++ cli/theia_open/cli.py
@@ -17,7 +17,7 @@ else:
 def main() -> None:
 
   argument_parser = argparse.ArgumentParser('theia-open')
-  argument_parser.add_argument("filename")
+  argument_parser.add_argument("filenames", nargs='+')
   argument_parser.add_argument("--wait", action="store_true")
   argument_parser.add_argument(
       "--url",
@@ -30,31 +30,34 @@ def main() -> None:
 
   options = argument_parser.parse_args()
 
-  filename = options.filename
-  if not os.path.isabs(options.filename):
-    filename = os.path.join(os.getcwd(), options.filename)
-  filename = os.path.normpath(filename)
-  if not os.path.isfile(filename):
-    argument_parser.error(f"File {options.filename} is not an existing file")
+  filenames = []
+  for filename in options.filenames:
+    if not os.path.isabs(filename):
+      filename = os.path.join(os.getcwd(), filename)
+    filename = os.path.normpath(filename)
+    #if not os.path.isfile(filename): # XXX keep original name
+    #  argument_parser.error(f"File {filename} is not an existing file")
+    filenames.append(filename)
 
   if not options.token:
     argument_parser.error(
         "--token or THEIA_OPEN_EDITOR_TOKEN environment variable is required")
   if not options.url:
-    argument_parser.error("--url or THEIA_URL environment variable is required")
+    argument_parser.error(
+        "--url or THEIA_URL environment variable is required")
 
   session = requests.Session()
   headers = {'X-Authentication-Token': os.environ['THEIA_OPEN_EDITOR_TOKEN']}
 
-  ret = requests.post(
-      url=urllib.parse.urljoin(options.url, "/api/openEditor/openFile"),
-      json={'filePath': filename},
-      headers=headers,
-  )
-  ret.raise_for_status()
+  for filename in filenames:
+    ret = requests.post(
+        url=urllib.parse.urljoin(options.url, "/api/openEditor/openFile"),
+        json={'filePath': filename},
+        headers=headers,
+    )
+    ret.raise_for_status()
 
-
-  def close_editor() -> None:
+  def close_editor(filename) -> None:
     ret = requests.post(
         url=urllib.parse.urljoin(options.url, "/api/openEditor/closeFile"),
         json={'filePath': filename},
@@ -65,10 +68,10 @@ def main() -> None:
     while True:
       delay = min(delay + 0.1, 1)
       time.sleep(delay)
-      if not is_file_open():
+      if not is_file_open(filename):
         return
 
-  def is_file_open() -> bool:
+  def is_file_open(filename) -> bool:
     ret = requests.post(
         url=urllib.parse.urljoin(options.url, "/api/openEditor/isFileOpen"),
         json={'filePath': filename},
@@ -79,8 +82,7 @@ def main() -> None:
     assert isinstance(is_open, bool)
     return is_open
 
-
-  def wait_for_editor_close(stdscr: _CursesWindow) -> int:
+  def wait_for_editor_close(stdscr: _CursesWindow, filename:str) -> int:
     curses.start_color()
     curses.use_default_colors()
     stdscr.clear()
@@ -97,20 +99,20 @@ def main() -> None:
     while True:
       k = stdscr.getch()
       if k == ord('q'):
-        close_editor()
+        close_editor(filename)
         return 0
       elif k == ord('a'):
-        close_editor()
+        close_editor(filename)
         return 1
       delay = min(delay + 0.1, 1)
       time.sleep(delay)
-      if not is_file_open():
+      if not is_file_open(filename):
         return 0
 
-
   if options.wait:
+    assert len(filenames) == 1 # TODO
     try:
-      sys.exit(curses.wrapper(wait_for_editor_close))
+      sys.exit(curses.wrapper(wait_for_editor_close, filenames[0]))
     except KeyboardInterrupt:
       close_editor()
       sys.exit(1)
