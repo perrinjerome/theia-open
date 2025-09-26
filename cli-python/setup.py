85f01be86215a5793f9f198e7e92d7aac7f789c9
Jérome Perrin <perrinjerome@gmail.com>
2025-09-26 23:48:00 +0000

    update python requires

--- cli-python/setup.py
+++ cli-python/setup.py
@@ -25,7 +25,7 @@ setup(
     ],
     keywords='theia editor',
     packages=find_packages(),
-    python_requires='>=3.6.*',
+    python_requires='>=3.9',
     install_requires=[
         'requests',
     ],
